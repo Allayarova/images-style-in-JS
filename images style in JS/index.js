b1919a96f4dbeb91b50ebb66b22f7a8474979bb5
Allayarova Mahliyo <117286548+Allayarova@users.noreply.github.com>
2023-02-10 07:48:30 +0300

    Add files via upload

---
+++ images style in JS/index.js
@@ -0,0 +1,38 @@
+function a()
+{
+    let elem1 = document.getElementById("pi");
+    elem1.style.fontSize = "20px";
+    elem1.style.color = " blue";
+    
+}
+function b()
+{
+    let elem2 = document.getElementById("h2");
+    elem2.style.fontSize = "25px";
+    elem2.style.color = " red";
+    elem2.style.fontFamily= " cursive";
+    elem2.style.textShadow = " 5px 5px";
+}
+function d()
+{
+    let elem3 = document.getElementById("rasm1");
+    let elem4 = document.getElementById("rasm2");
+    let elem5 = document.getElementById("rasm3");
+    let elem6 = document.getElementById("rasm4");
+    
+    elem3.style.boxShadow = "5px 10px 5px 5px";
+    elem3.style.borderRadius = "20px"
+    elem4.style.boxShadow = "5px 10px 5px 5px";
+    elem4.style.borderRadius = "20px"
+    elem5.style.boxShadow = "5px 10px 5px 5px";
+    elem5.style.borderRadius = "20px"
+    elem6.style.boxShadow = "5px 10px 5px 5px";
+    elem6.style.borderRadius = "20px"
+
+}
+function e()
+{
+    let j = document.getElementById('reklama');
+    j.style.backgroun="red"
+}
+
